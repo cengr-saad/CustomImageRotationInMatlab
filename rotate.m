b4b6f921b6907441e6fccda2dbbb8096a934073b
Saad <111654330+cengr-saad@users.noreply.github.com>
2023-04-07 23:46:00 +0500

    Update rotate.m

--- rotate.m
+++ rotate.m
@@ -38,24 +38,3 @@ for i=1 : size(new,1)
         end
     end
 end
-% size_mat = size(mat);
-% center = round(size_mat/2);
-% for i=1 : size(mat,1)
-%     for j=1 : size(mat,2)
-%         coord = [i;j];
-%         r_coord = round(X*coord);
-%         if r_coord(1)<1
-%             r_coord(1)=1;
-%         end
-%         if r_coord(2)<1
-%             r_coord(2)=1;
-%         end
-%         if r_coord(1)>size(mat,1)
-%             r_coord(1)=size(mat,1);
-%         end
-%         if r_coord(2)>size(mat,2)
-%             r_coord(2)=size(mat,2);
-%         end
-%         new(r_coord(1), r_coord(2)) = uint8(mat(coord(1), coord(2)));
-%     end
-% end
